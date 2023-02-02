0b104fc794a55cc6f40bfd7952d7616eb16ebe4b
unknown <amarjeet@baruzotech.com>
2023-02-02 18:30:11 +0530

    update

--- books/books.js
+++ books/books.js
@@ -16,7 +16,7 @@ app.post('/book', (req, res) => {
   const data = req.body;
   Book.create(data)
     .then(() => {
-      res.status(200).send('New Book added successfully!');
+      res.status(201).send('New Book added successfully!');
     })
     .catch((err) => {
       res.status(500).send('Internal Server Error!');
@@ -27,7 +27,7 @@ app.get('/books', (req, res) => {
   Book.find()
     .then((books) => {
       if (books.length !== 0) {
-        res.json(books);
+        res.status(200).send(books);
       } else {
         res.status(404).send('Books not found');
       }
